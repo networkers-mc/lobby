2f9ed51bda2295aeadc50e7f91124455fb23385a
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2022-03-13 19:24:17 +0100

    Bind WorldController module

--- src/main/java/es/networkersmc/lobby/LobbyManifest.java
+++ src/main/java/es/networkersmc/lobby/LobbyManifest.java
@@ -3,6 +3,7 @@ package es.networkersmc.lobby;
 import com.google.inject.Provides;
 import es.networkersmc.dendera.inject.Manifest;
 import es.networkersmc.lobby.player.PlayerController;
+import es.networkersmc.lobby.world.WorldController;
 import org.bukkit.Server;
 import org.bukkit.World;
 
@@ -11,6 +12,7 @@ public class LobbyManifest extends Manifest {
     @Override
     protected void configure() {
         bindModule(PlayerController.class);
+        bindModule(WorldController.class);
     }
 
     @Provides
